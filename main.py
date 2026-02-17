3832cbc5238827bf475678fb32162584959aee69
Miguel-SMR <mialvarezal1.bec@acciona.com>
2026-02-17 15:25:54 +0100

    Reloj Pico

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, RTC
 from ssd1306 import SSD1306_I2C
 import time
  
 pin = Pin('LED', Pin.OUT)  # Pin integrado del LED en Raspberry Pi Pico
-
+rtc = RTC()  # Inicializar el reloj real time (RTC)
 # --- CONFIGURACIÓN DE PANTALLA ---
 WIDTH = 128
 HEIGHT = 64
-# Asegúrate de que los pines coincidan con tu placa (ej. Raspberry Pi Pico: scl=17, sda=16)
+
 i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=400000)
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
-def parpadear_led(veces, intervalo):
-    for _ in range(veces):
-        pin.value(1)  # Encender LED
-        time.sleep(intervalo)
-        pin.value(0)  # Apagar LED
-        time.sleep(intervalo)
-
-def ensenar_contenido(sleep_time):
-    oled.show()  # Mostrar cambios
-    time.sleep(sleep_time)
-
-def interaccion_pantalla():
-    oled.fill(1)  # Llenar pantalla de blanco
-    ensenar_contenido(1)
-    oled.fill(0)  # Limpiar pantalla
-    ensenar_contenido(1)
-    oled.text("Hola, Mundo!", 0, 0)  # Escribir texto en la pantalla
-    ensenar_contenido(1)
-    oled.text("Raspberry Pico", 0, 10)  # Escribir más texto
-    ensenar_contenido(1)
-    oled.fill(0)  # Limpiar pantalla
-    ensenar_contenido(1)
-    oled.rect(10, 10, 50, 30, 1)  # Dibujar un rectángulo
-    ensenar_contenido(1)
-    oled.pixel(20, 20, 1)  # Dibujar un pixel
-    ensenar_contenido(1)
-    oled.fill(0)  # Limpiar pantalla
-    oled.show()  # Mostrar cambios
+rtc.datetime((2026, 2, 17, 2, 15, 21, 0, 0))  # Establecer fecha y hora (año, mes, día, día de la semana, hora, minuto, segundo, milisegundo)
 
 while True:
-    parpadear_led(7, 0.2)  # Parpadear el LED 5 veces con un intervalo de 0.5 segundos
-    interaccion_pantalla()  # Interactuar con la pantalla OLED
-    time.sleep(2)  # Esperar antes de repetir el ciclo
+    fecha = rtc.datetime()  # Obtener la fecha y hora actual
+    hora = f"{fecha[4]:02d}:{fecha[5]:02d}:{fecha[6]:02d}"  # Formatear la hora
+    
+    oled.fill(0)  # Limpiar la pantalla
+    oled.text("Reloj Pico", 30, 10, 1)
+
+    oled.text(hora, 30, 30, 1)  # Mostrar la hora en la pantalla
+    oled.text(f"{fecha[2]:02d}/{fecha[1]:02d}/{fecha[0]}", 30, 50, 1)  # Mostrar la fecha en formato DD/MM/YYYY
+    oled.show()  # Actualizar la pantalla
+    pin.toggle()  # Alternar el estado del LED integrado
+    time.sleep(1)  # Esperar 1 segundo antes de actualizar la hora nuevamente
+
